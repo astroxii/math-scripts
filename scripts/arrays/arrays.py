5a6340803f386118acee8231f9c9c6cbcad93e23
astroxii <67867895+astroxii@users.noreply.github.com>
2022-06-01 08:00:28 -0300

    Update arrays.py

--- scripts/arrays/arrays.py
+++ scripts/arrays/arrays.py
@@ -7,9 +7,23 @@ from math import sqrt
 class Array(list):
     def __init__(self, items: list):
         self.extend(items)
+        self.sort()
 
-    def mean(self) -> float:
+    def mean(self) -> float | int:
         return sum(self) / len(self)
+
+    def median(self) -> float | int:
+        return self[(len(self)-1)//2] if len(self) % 2 == 1 else ((self[(len(self))//2]+self[(len(self))//2])/2)
+
+    def mode(self) -> list | float | int | None:
+        modes = []
+
+        for i in range(len(self)):
+            if i+1 < len(self):
+                if self[i] == self[i+1] and self[i] not in modes:
+                    modes.append(self[i])
+
+        return modes if len(modes) > 1 else modes[0] if len(modes) == 1 else None
 
     def deviation(self) -> list:
         d = []
@@ -19,7 +33,7 @@ class Array(list):
 
         return d
 
-    def variance(self) -> float:
+    def variance(self) -> float | int:
         sdv = []
 
         for d in self.deviation():
@@ -29,9 +43,12 @@ class Array(list):
 
         return v
 
-    def std_deviation(self) -> float:
+    def std_deviation(self) -> float | int:
         return sqrt(self.variance())
 
 
-myArray = Array(items=[21, 4, 22, 15, 19, 18, 15, 7])
+myArray = Array(items=[21, 4, 22, 12, 19, 18, 15, 7, 21, 9, 7])
 print(myArray)
+print(myArray.median())
+print(myArray.mode())
+print(myArray.std_deviation())
