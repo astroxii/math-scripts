3deea9b4864a262b31b84178f5a5c55d3535eccc
astroxii <67867895+astroxii@users.noreply.github.com>
2022-06-10 18:25:04 -0300

    Add some, move folders

---
+++ scripts/statistic_probability/poisson_distribution.py
@@ -0,0 +1,15 @@
+import math
+
+# Script by astroxii @ 2022
+# Have fun!
+
+
+def poisson_distribution(_lambda: int, x: int) -> float:
+    return ((_lambda ** x) * (math.e ** -_lambda)) / math.factorial(x)
+
+
+
+# Chances of 25 cars be at a garage which's daily mean is 17 is...
+
+prob = poisson_distribution(17, 25) * 100
+print(f"~ {prob:.4}%")
